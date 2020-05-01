04903eb62431ad07c5977c92b50e4632862a29d0
Roman Wanner <roman_wanner@rowa-digital.ch>
2020-05-01 09:05:09 -0300

    Update PHP example
    Added comments

--- src/examples/php/index.php
+++ src/examples/php/index.php
@@ -6,17 +6,17 @@
  *
  * Copyrights: Roman Wanner 2020
  */
-include('../../codes/php/Countries.php');
-use Rowa\Countries as Countries;
+include('../../codes/php/Countries.php'); //Includes the Countries.php file
+use Rowa\Countries as Countries; //loads the class from the file and sets it as "Countries"
 
-$countries = new Countries();
+$countries = new Countries(); //creates a new instance of "RoWa\Countries"
 ?>
 <html>
 <?php
 echo '<b>Object:</b><br>';
-print_r($countries->getCountry('ch'));
+print_r($countries->getCountry('ch')); //prints the whole content of the array for the country "ch"
 echo '<br><br>';
 echo '<b>Single Key (Size):</b><br>';
-echo $countries->getCountry('ch')->Size;
+echo $countries->getCountry('ch')->Size; //Gets a single key "Size" and returns its value
 ?>
 </html>
