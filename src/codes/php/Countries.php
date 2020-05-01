38b71eeec34cbf1c42ef64fcb3cbe9d9e7fb2bb8
Roman Wanner <roman_wanner@rowa-digital.ch>
2020-05-01 09:54:58 -0300

    Update Countries.php
    Added Cities class

--- src/codes/php/Countries.php
+++ src/codes/php/Countries.php
@@ -8,13 +8,41 @@
  */
 
 namespace Rowa{
+	class Cities {
+		public $data = [];
+		
+		public function __construct($dir = ""){
+			if(empty($dir)){
+				$dir = "../../cities/";
+			}
+			$countries = new Countries();
+			foreach($countries->data as $country){
+				$fname = $dir.$country->ISO2.".json";
+				if(file_exists($fname)){
+					$fcontent = file_get_contents($fname);
+					//REMOVE UNWANTED CHARS
+					for ($i = 0; $i <= 31; ++$i) { 
+						$fcontent = str_replace(chr($i), "", $fcontent); 
+					}
+					$fcontent = str_replace(chr(127), "", $fcontent);
+					if (0 === strpos(bin2hex($fcontent), 'efbbbf')) {
+					   $fcontent = substr($fcontent, 3);
+					}
+					//REMOVE UNWANTED CHARS END
+					$jsondata = json_decode( $fcontent );
+					$data[$country->ISO2] = $jsondata;
+				}
+			}
+		}
+	}
+	
 	class Countries {
 		
 		public $data;
 		
 		public function __construct($fname = ""){
 			if(empty($fname)){
-				$fname = './rowa/countries/countries.json';
+				$fname = './countries.json';
 			}
 			$fcontent = file_get_contents($fname);   
 			
