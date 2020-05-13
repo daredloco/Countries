deeab06b7639ca7fe466aacc27c781958fadab15
Roman Wanner <roman_wanner@rowa-digital.ch>
2020-05-13 10:48:42 -0300

    Bugfix PHP code & example

--- src/codes/php/Countries.php
+++ src/codes/php/Countries.php
@@ -36,8 +36,8 @@ namespace Rowa{
 		}
 		
 		public function getCitiesFromCountry($key){
-			if(array_key_exists($key)){
-				return $data[$key];
+			if(array_key_exists($key, $this->data)){
+				return $this->data[$key];
 			}
 			return false;
 		}
